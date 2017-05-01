e16462aa4c45cc2e63a2c44477419b2f14ed6b44
wl02227976 <b04201043@ntu.edu.tw>
2017-05-01 17:29:08 +0800

    Add files via upload

--- readraw.m
+++ readraw.m
@@ -1,9 +1,20 @@
-function readraw(n)
+function readraw(n,m)
 A=dir(strcat('¤ôªd/',num2str(n),'/'));
-N=xlsread(strcat('¤ôªd/',num2str(n),'/',A(4).name),'2:2');
+N=xlsread(strcat('¤ôªd/',num2str(n),'/',A(m).name),'2:2');
 L=length(N);
-   for i=1:L
+if m==4|m==9
+      for i=1:L
+          x(i)=i-1;
+      end
+   plot(x,N);
+else
+   g=L/2;
+   for i=1:g
        x(i)=i-1;
+       a=2*i-1
+       b(i)=N(a);
    end
-plot(x,N);
+   plot(x,b);
+   
+end   
 end
