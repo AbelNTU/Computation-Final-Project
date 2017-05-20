d954d9ba5c750a9e14dd77b6baf49e0358806507
wl02227976 <b04201043@ntu.edu.tw>
2017-05-21 00:01:55 +0800

    Add files via upload

---
+++ alldrawtest.m
@@ -0,0 +1,24 @@
+function alldrawtest(o,n,m,l)
+company=dir('公司種類/');
+A=dir(strcat('公司種類/',company(o).name,'/',num2str(n),'/'));
+[N,T,data]=xlsread(strcat('公司種類/',company(o).name,'/',num2str(n),'/',A(m).name));
+[e,f]=size(T);
+f=f-1;
+if m==3||m==6
+    x = zeros(f);
+      for i=1:f
+          x(i)=i-1;
+          y(i)=N(l-1,i);
+      end
+   plot(x,y);
+else
+   f=f/2;
+   x = zeros(f);
+   for i=1:f
+       x(i)=i-1;
+       a=2*i-1;
+       y(i)=N(l-1,a);
+   end
+   plot(x,y);   
+end   
+end
