8594c6db6951c102aba6a09d42f86043c7d6ec8c
AbelNTU <B04201032@ntu.edu.tw>
2017-06-17 22:53:47 +0800

    新增excel檔案

--- GUI/GUI.m
+++ GUI/GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 17-Jun-2017 08:42:29
+% Last Modified by GUIDE v2.5 17-Jun-2017 22:05:10
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -114,7 +114,6 @@ function popupmenu2_Callback(hObject, eventdata, handles)
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu2 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu2
 
-
 % --- Executes during object creation, after setting all properties.
 function popupmenu2_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to popupmenu2 (see GCBO)
@@ -133,7 +132,6 @@ function checkbox1_Callback(hObject, eventdata, handles)
 % hObject    handle to checkbox1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of checkbox1
 
 
@@ -266,43 +264,80 @@ target = categorys{get(handles.popupmenu1,'Value')};
 companys = get(handles.popupmenu2,'String'); 
 company = companys{get(handles.popupmenu2,'Value')};
 num = find(strcmp(companys,company));
+%每月營收
+report_e = get(handles.checkbox1,'Value');
+%每月股價
+report_p = get(handles.checkbox2,'Value');
+
 switch subcat
     case '流動資產'
         labels = {'預付款項','應收帳款淨額','其他流動資產','其他應收款淨額','存貨','現金及約當現金'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        h = pie(handles.axes1,data(:,num));
+        h1 = pie(handles.axes2,sum(data,2));
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     case '非流動資產'
         labels = {'備供出售金融資產－非流動淨額','採用權益法之投資淨額','不動產、廠房及設備',...
           '投資性不動產淨額','無形資產','其他非流動資產'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        pie(handles.axes1,data(:,num))
+        pie(handles.axes2,sum(data,2))
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     case '流動負債'
         labels = {'短期借款','應付短期票券','應付票據','應付帳款','其他應付款','當期所得稅負債','其他流動負債'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        pie(handles.axes1,data(:,num))
+        pie(handles.axes2,sum(data,2))
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     case '非流動負債'
         labels = {'長期借款','遞延所得稅負債','其他非流動負債'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        pie(handles.axes1,data(:,num))
+        pie(handles.axes2,sum(data,2))
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     case '營業費用'
         labels = {'推銷費用','管理費用','研究發展費用'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        pie(handles.axes1,data(:,num))
+        pie(handles.axes2,sum(data,2))
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     case '其他綜合損益(淨額)'
         labels = {'國外營運機構財務報表換算之兌換差額','備供出售金融資產未實現評價損益',...
           '現金流量避險','確定福利計畫精算利益（損失）','採用權益法認列之關聯企業及合資之其他綜合損益之份額合計',...
           '其他綜合損益'};
         data = xlsread(strcat(report_cat,'/',target,'/',subcat,'/',year,'0',season,'.xlsx'));
-        pie(handles.axes1,data(:,num),labels)
-        pie(handles.axes2,sum(data,2),labels)
+        pie(handles.axes1,data(:,num))
+        pie(handles.axes2,sum(data,2))
+        legend(handles.axes1,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
+        legend(handles.axes2,labels,'Location','eastoutside','Orientation','vertical','FontSize',12);
     otherwise
         labels = categorical({'營業活動之淨現金流入（流出）','投資活動之淨現金流入（流出）','籌資活動之淨現金流入（流出）'});
         data = xlsread(strcat(report_cat,'/',target,'/',year,'0',season,'.xlsx'));
         bar(handles.axes1,labels,data(:,num))
         bar(handles.axes2,labels,sum(data,2))
+end
+title(handles.axes1,company,'Color','r','FontSize',24);
+title(handles.axes2,target,'Color','r','FontSize',24);
+if report_e == 1
+  excelname=dir(strcat('excel檔/',target,'.excel/',company));
+  [N,T,data]=xlsread(strcat('excel檔/',target,'.excel/',company,'/',excelname(4).name));
+  [e,f]=size(T);
+  f=f-1;
+  x = zeros(f);
+      for i=1:f
+          x(i)=i-1;
+          y(i)=N(2,i);
+      end
+   plot(handles.axes2,x,y);
+end    
+if report_p == 1    
+  excelname=dir(strcat('excel檔/',target,'.excel/',company));
+  data=xlsread(strcat('excel檔/',target,'.excel/',company,'/',excelname(8).name));
+  
+  plot(handles.axes1,data);
 end
